2b720f503772971a5004a0694926f5597949d690
Adrián García <adriagtrias@gmail.com>
2023-07-18 20:37:53 +0200

    Inital checkings. Added config file.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            menuStrip1 = new MenuStrip();
+            configToolStripMenuItem = new ToolStripMenuItem();
+            changeModPathToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(158, 92);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { configToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(359, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // configToolStripMenuItem
+            // 
+            configToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { changeModPathToolStripMenuItem });
+            configToolStripMenuItem.Name = "configToolStripMenuItem";
+            configToolStripMenuItem.Size = new Size(55, 20);
+            configToolStripMenuItem.Text = "Config";
+            // 
+            // changeModPathToolStripMenuItem
+            // 
+            changeModPathToolStripMenuItem.Name = "changeModPathToolStripMenuItem";
+            changeModPathToolStripMenuItem.Size = new Size(180, 22);
+            changeModPathToolStripMenuItem.Text = "Change Mod Path";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(359, 188);
+            Controls.Add(label1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem configToolStripMenuItem;
+        private ToolStripMenuItem changeModPathToolStripMenuItem;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,61 @@
+using EasyConfig;
 namespace Snowrunner_Parcher
 {
     public partial class Form1 : Form
     {
+        private Config cf = new();
+        private string Token;
+        private bool IsIniConfigLoaded = false;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckConfig();
+
+            CheckForUpdates();
+        }
+        private void CheckConfig()
+        {
+            if (cf.ConfigData["Game"]["ModsPath"] == null || cf.ConfigData["Game"]["ModsPath"] == "") IniConfig();
+
+            changeModPathToolStripMenuItem.ToolTipText = cf.ConfigData["Game"]["ModsPath"];
+        }
+
+        private void IniConfig()
+        {
+            if (!ChangeModPath())
+            {
+                MessageBox.Show("In order to apply the patch, we need to configure the destination path for the pack mods folder.", "Atention", MessageBoxButtons.OK);
+                Close();
+            }
+            IsIniConfigLoaded = true;
+
+        }
+
+        private bool ChangeModPath()
+        {
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = Path.GetDirectoryName(Application.ExecutablePath);
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    cf.ConfigData["Game"]["ModsPath"] = openFileDialog.FileName;
+                }
+                else return false;
+            }
+
+            return true;
+        }
+        private async void CheckForUpdates()
+        {
+            Token = await GetToken.GetTokenFromRequest();
+            //label1.Text = pepi;
         }
     }
 }
